9a9b46b6a0432b42bcfc908dd278f7178da384d6
Nathalia Ramos <nathii.siilva@gmail.com>
2023-11-02 20:58:41 -0300

    Arrumando diretrizes de validação

--- src/app/products/product-form/product-form.component.ts
+++ src/app/products/product-form/product-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, NonNullableFormBuilder } from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
@@ -16,6 +16,9 @@ export class ProductFormComponent implements OnInit {
 
   form!: FormGroup;
 
+  numRegex = /^-?\d*[.,]?\d{0,2}$/;
+
+
   constructor(private formBuilder: NonNullableFormBuilder,
     private service: ProductsService,
     private route: ActivatedRoute,
@@ -27,9 +30,9 @@ export class ProductFormComponent implements OnInit {
     const product: Product = this.route.snapshot.data['product'];
     this.form = this.formBuilder.group({
       id: [product.id],
-      name: [product.name],
-      barCode: [product.barCode],
-      price: [product.price]
+      name: [product.name, [Validators.maxLength(50)]],
+      barCode: [product.barCode, [Validators.required, Validators.maxLength(13)]],
+      price: [product.price, [Validators.pattern(this.numRegex)]]
     })
   }
 
@@ -51,4 +54,26 @@ export class ProductFormComponent implements OnInit {
   private onError() {
     this.snackBar.open("Erro ao salvar o produto.", '', { duration: 5000 });
   }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    if (field?.hasError('pattern')) {
+      return 'Campo deve ser numérico e apenas com 2 cadas decimais';
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength: number = field.errors ? field.errors['maxlength']['requiredLength'] : 50;
+      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
+    }
+
+
+
+    return 'Campo Inválido';
+  }
+
 }
